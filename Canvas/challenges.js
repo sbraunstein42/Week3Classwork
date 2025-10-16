2d5c49b77d4e7e0b1ec2a19463026a78f5f3b6f4
sbraunstein42 <sbraunstein@neit.edu>
2025-10-15 23:52:19 -0400

    Fixed styles

--- Canvas/challenges.js
+++ Canvas/challenges.js
@@ -4,8 +4,8 @@
 
 // Challenge 1: Set up your canvas
 // Look for your <canvas> element to your html file, with id "myCanvas". See it?
-// Change the size of the "myCanvas" style in styles.css to be 500px wide by 400px high.
-// Give it a "gray" border of 5 pixels.
+// Change the size of the canvas to width = 500 and height = 400.
+// In styles.css, give it a "gray" border of 5 pixels.
 
 // No javascript for challenge 1.
 
@@ -14,9 +14,12 @@
 // Draw a filled rectangle at x=30, y=30 with width=100, height=50
 // Fill it with the color "#EA9A4A"!
 
+let canvas = document.getElementById("myCanvas");
+let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 
-
-
+//draw a filled rect
+pencil.fillStyle = "#EA9A4A";     // color to fill the rectangle
+pencil.fillRect(30, 30, 100, 50); // x, y, width, height
 
 
 
@@ -24,11 +27,13 @@
 
 
 // Challenge 3: Draw an outlined rectangle
-// Draw a rectangle outline at x=150, y=30 with width=120, height=70
-// Make the outline color red and thickness 4.
+// Draw a rectangle outline at x=150, y=30 with width=200, height=250
+// Make the outline color a nice red, and give it a thickness of 20 pixels.
+// Careful! If you do it in the wrong order, you'll get...the default style...
 
-
-
+pencil.strokeStyle = "#a33d3dff";
+pencil.lineWidth = 20;
+pencil.strokeRect(150, 30, 200, 250); //x, y, width, height again
 
 
 
@@ -38,10 +43,13 @@
 
 // Challenge 4: Draw a circle
 // Draw a circle at x=100, y=150 with radius 40
-// Fill it with green color.
+// Fill it with a minty green color, for freshness.
 
-
-
+pencil.beginPath(); //begin your path
+pencil.arc(100, 150, 40, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+pencil.fillStyle = "#93dbbcff";
+pencil.fill();
+pencil.closePath();
 
 
 
@@ -50,10 +58,24 @@
 
 // Challenge 5: Draw a line
 // Draw a line from top-left (0,0) to bottom-right (canvas width, canvas height)
-// Make it blue and 3 pixels thick.
+// Make it purple and 3 pixels thick.
 
 
+// Start a new path
+pencil.beginPath();
 
+// Move the "pencil" to the starting point (x=50, y=50)
+pencil.moveTo(0, 0);
+
+// Draw a line to one point (x=500, y=400)
+pencil.lineTo(500, 400);
+
+// Add some style!
+pencil.strokeStyle = "purple";
+pencil.lineWidth = 3;
+
+// Render the line on the canvas!
+pencil.stroke();
 
 
 
@@ -62,10 +84,24 @@
 
 // Challenge 6: Draw a triangle
 // Draw a filled triangle with points at (200,150), (250,250), and (150,250)
-// Fill it with any color and optionally outline it.
+// Fill it with a calming pink color!
 
 
+// Start a new shape
+pencil.beginPath();
 
+// Move to the first point
+pencil.moveTo(200, 150);
+
+// Draw lines to additional points
+pencil.lineTo(250, 250);
+pencil.lineTo(150, 250);
+
+pencil.fillStyle = "#ffc5f8ff";
+pencil.fill();
+
+// Close the path (connects last point to first)
+pencil.closePath();
 
 
 
@@ -79,10 +115,9 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_text
 // Ready, go!
 
-
-
-
-
+pencil.fillStyle = "#5f6d1bff";
+pencil.font = "48px serif";
+pencil.fillText("Bananas Bunched", 10, 370);
 
 
 
